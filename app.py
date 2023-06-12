5b2520b88ca1bd9202342ee3bf1d5388e8350b53
sangramdhurve <sangramdhurve96@gmail.com>
2023-06-12 16:32:41 +0530

    host and port chenged

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def home():
 @app.route("/train", methods=['GET','POST'])
 @cross_origin()
 def trainRoute():
-    os.system("dvc repro") # python3 main.py also you can give here.
+    os.system("python3 main.py") # python3 main.py also you can give here.
     return "Training done successfully!"
 
 
@@ -44,6 +44,6 @@ def predictRoute():
 if __name__ == "__main__":
     clApp = ClientApp()
     app.run(host='0.0.0.0', port=8080) #local host
-    # app.run(host='0.0.0.0', port=8080) #for AWS
-    # app.run(host='0.0.0.0', port=80) #for AZURE
+    # # app.run(host='0.0.0.0', port=8080) #for AWS
+    # # app.run(host='0.0.0.0', port=80) #for AZURE
 
